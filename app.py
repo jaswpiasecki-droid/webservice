24b15d09e3defc910f28dd1c49ab05de6ae9588f
Jan Piasecki <jas.w.piasecki@gmail.com>
2026-09-24 20:41:42 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+odp = 'Odopwiedź'
+
+@app.route("/strona/")
+def hello_world():
+    return '<p>Hej</p>'
+
+
+def cos():
+    return render_template('strona.html')
